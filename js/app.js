45285d4873a2b62c4e5808d158aa6dfe9b71bf14
Rivers-Shall <riversshall@qq.com>
2020-06-11 15:44:16 +0800

    init: app without webpack

---
+++ js/app.js
@@ -0,0 +1,3 @@
+button.addEventListener('click', function () {
+    graph.style.display = graph.style.display === 'none' ? 'block' : 'none'
+})
---
+++ js/dom-loader.js
@@ -0,0 +1,2 @@
+var button = document.querySelector("#toggle-button")
+var graph = document.querySelector("#toggled-graph")
